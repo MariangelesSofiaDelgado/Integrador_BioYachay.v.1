f3c9ea3833685996d1eb80c56f792b802a34406c
valentinoZZZ-star <valentinoloarte123@gmail.com>
2026-05-29 19:56:08 -0500

    interfaz de coordinacion

--- app/modulo/coordinacion/styles/stylesindex.tsx
+++ app/modulo/coordinacion/styles/stylesindex.tsx
@@ -1,69 +1,157 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default StyleSheet.create({
-  contenedor: {
+  page: {
     flex: 1,
-    backgroundColor: "#ececec",
-    padding: 20,
+    backgroundColor: "#ededed",
+  },
+  
+  /* --- CAPA ABSOLUTA DE ANIMACIÓN --- */
+  capaAnimacionGlobal: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 0,
+  },
+  frutaAnimada: {
+    fontSize: 32,
+    position: "absolute",
+  },
+
+  /* --- SECCIÓN SUPERIOR (60% DEL ALTO) --- */
+  zonaSuperior: {
+    flex: 1,
     justifyContent: "flex-start",
+    zIndex: 2,
   },
   titulo: {
-    fontSize: 36,
-    fontWeight: "800",
-    color: "#2f5279",
-    textAlign: "center",
-    marginTop: 30,
-    marginBottom: 20,
-  },
-  espacioJuego: {
-    height: 200,
-    borderWidth: 2,
-    borderColor: "#c9c9c9",
-    borderRadius: 14,
-    backgroundColor: "#f8f8f8",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 30,
-  },
-  textoEspacio: {
-    fontSize: 20,
-    color: "#666666",
-    textAlign: "center",
-  },
-  cajaObjetivo: {
-    backgroundColor: "#d9f9d9",
-    padding: 16,
-    borderRadius: 10,
-    marginBottom: 20,
-  },
-  cajaInstrucciones: {
-    backgroundColor: "#d9e9f9",
-    padding: 16,
-    borderRadius: 10,
-    marginBottom: 30,
-  },
-  subtitulo: {
-    fontSize: 24,
+    fontSize: 38,
     fontWeight: "700",
-    marginBottom: 8,
+    color: "#e93232",
     textAlign: "center",
-    color: "#2f5279",
+    marginTop: 35,
+    marginBottom: 4,
   },
-  textoCaja: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#333333",
+  tituloLinea: {
+    width: 230,
+    height: 3,
+    backgroundColor: "#e93232",
+    alignSelf: "center",
+    borderRadius: 999,
+    marginTop: 3,
+    marginBottom: 5,
+  },
+  espacioGrisLibre: {
+    flex: 1,
+    width: "100%",
+    position: "relative",
+    justifyContent: "flex-end",
+    paddingBottom: 25, 
+  },
+  canastaAnimada: {
+    fontSize: 46,
+    position: "absolute",
+    bottom: 20, 
+    // Quitamos cualquier valor horizontal fijo aquí para que dependa 100% de la animación
+  },
+
+  /* --- TARJETA BLANCA (40% DEL ALTO REAL) --- */
+  contenedor: {
+    height: SCREEN_HEIGHT * 0.40, 
+    backgroundColor: "#ffffff",
+    paddingTop: 18,
+    paddingHorizontal: 24,
+    paddingBottom: 15,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    justifyContent: "space-between", 
+    
+    zIndex: 10,
+    elevation: 8, 
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: -5 },
+    shadowOpacity: 0.15, 
+    shadowRadius: 6,
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 5,
+  },
+  objectContenedor: {
+    alignSelf: "flex-start",
+    width: "75%",
+    backgroundColor: "#f6f6f6",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+    marginLeft: 5,
+    top: -10, 
+  },
+  objetivoContenedor: {
+    alignSelf: "flex-start",
+    backgroundColor: "#7dc123",
+    paddingVertical: 5,
+    paddingHorizontal: 14,
+    borderRadius: 8,
+    elevation: 1,
+  },
+  objetivoTitulo: {
+    fontSize: 18, // Ajustado a 18
+    fontWeight: "600",
+    color: "#fff",
+  },
+  objetivoDescripcion: {
+    fontSize: 18, // Ajustado a 18
+    color: "#333",
+  },
+  instruccionesContenedor: {
+    alignSelf: "flex-start",
+    paddingVertical: 5,
+    paddingHorizontal: 14,
+    borderRadius: 8,
+    backgroundColor: "#549be5",
+    elevation: 1,
+    marginTop: -5,
+  },
+  instruccionesTitulo: {
+    fontSize: 18, // Ajustado a 18
+    fontWeight: "600",
+    color: "#fff",
+  },
+  indicaciones: {
+    marginVertical: 2,
+    paddingHorizontal: 5,
+  },
+  indicacion: {
+    fontSize: 18, // Ajustado a 18 para máxima legibilidad de niños y abuelos
+    marginBottom: 5,
+    color: "#444",
+    fontWeight: "500",
+  },
+  botonBase: {
+    alignSelf: "center",
+    backgroundColor: "#ce540e",
+    borderRadius: 20,
+    paddingVertical: 2,
+    marginBottom: 5,
   },
   botonIniciar: {
     alignSelf: "center",
-    backgroundColor: "#2f5279",
-    paddingHorizontal: 32,
-    paddingVertical: 14,
-    borderRadius: 12,
+    backgroundColor: "#faa638",
+    paddingHorizontal: 75,
+    paddingVertical: 8,
+    borderRadius: 20,
+    position: "relative",
+    top: -5,
+    elevation: 3,
   },
   textoBoton: {
-    color: "#ffffff",
-    fontSize: 22,
-    fontWeight: "700",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
